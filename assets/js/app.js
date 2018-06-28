742d9b9d5c4b414da17d32f121ec0ddabc7f20da
Abdulrahman <25ambat25@gmail.com>
2018-06-28 11:53:27 +0100

    added support for index db

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,3 @@
-const dummmy = "aminu";
 const app = new Vue({
     el: '#app',
     data: {
@@ -33,17 +32,36 @@ const app = new Vue({
                         myDb.then(function (db) {
                             var tx = db.transaction('convertion-rate', 'readwrite')
                             var convertionRateStore = tx.objectStore('convertion-rate')
-                            convertionRateStore.clear()
-                            convertionRateStore.put(res,'convertion rate');
+                            convertionRateStore.put(Object.values(res)[0],Object.keys(res)[0]);
+                            convertionRateStore.put(Object.values(res)[1],Object.keys(res)[1]);
                             return tx.complete
                         })
                         .then(()=>{
+                            self.error_message = ""
                             console.log('convertion rate is set')
                         })
                     })
                     .catch(err=>{
                         console.error(err);
                         console.log('error fetching convertion rate')
+                        console.log('dont know why some things')
+                        myDb.then(function (db) {
+                            var tx = db.transaction('convertion-rate', 'readwrite')
+                            var convertionRateStore = tx.objectStore('convertion-rate')
+                            // convertionRateStore.clear()
+                            convertionRateStore.get(from_to).then(val=>{self.convert_from_rate = val});
+                            convertionRateStore.get(to_from).then(val=>{self.convert_to_rate = val});
+                            return tx.complete
+                        })
+                        .then(()=>{
+                            console.log('fetched from db')
+                            self.error_message = ""
+                            console.log("from to",self.convert_from_rate)
+                            if(self.convert_from_rate == undefined){self.error_message = "Unable to fetch Conversion rate"}
+                        })
+                        .catch(()=>{self.error_message = "Unable to fetch Conversion rate"})
+                        console.log('not going trough')
+                        
                     })
             }
         },
@@ -60,7 +78,7 @@ const app = new Vue({
         },
         toChange() {
             this.from_val = (Number(this.to_val) * Number(this.convert_from_rate)).toFixed(2)
-        },
+        }
     },
     created() {
         const self = this;
@@ -92,12 +110,12 @@ const app = new Vue({
                     
                     return tx.complete;
                 }).then(function (data) {
-                    console.log('added to database')
+                    console.log('fetched from database')
                 })
             })
 
     },
-    mounted() {
-        console.log('mounted method')
+    mounted(){
+        console.log('mounted')
     }
 })
--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,4 @@
-let cacheName = 'cur-conv-v15';
-// self.addEventListener('install', e => {
-//   const timeStamp = Date.now();
-//   e.waitUntil(
-//     caches.open(cacheName).then(cache => {
-//       return cache.addAll([
-//         `/`,
-//         `/index.html`,
-//         `/assets/css/main.css`,
-//         '/assets/js/app.js'
-//       ])
-//           .then(() => self.skipWaiting());
-//     })
-//   );
-// });
+let cacheName = 'cur-conv-v1';
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(cacheName)
@@ -26,32 +12,11 @@ self.addEventListener('install', event => {
       })
   )
 })
+
 self.addEventListener('activate', event => {
   event.waitUntil(self.clients.claim());
 });
 
-// self.addEventListener('fetch', event => {
-//     event.respondWith(
-//         caches.open(cacheName)
-//             .then(cache=>{
-//                 return cache.match('/')
-//             })
-//             .then(response => {
-//                 return response || fetch(event.request);
-
-//             })
-//             .catch(err => fetch(event.request))
-//     )
-// })
-// self.addEventListener('fetch', event => {
-//   event.respondWith(
-//     caches.open(cacheName)
-//       .then(cache => cache.match(event.request, {ignoreSearch: true}))
-//       .then(response => {
-//       return response || fetch(event.request);
-//     })
-//   );
-// });
 addEventListener('fetch', function (event) {
   event.respondWith(
     caches.match(event.request)
